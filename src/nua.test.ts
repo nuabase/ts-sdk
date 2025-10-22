bf974d9038194698beeace4207caf872377cc6e2
Jasim A Basheer <jasim.ab@gmail.com>
2025-10-22 14:08:28 +0530

    Add test for cast/value

--- src/nua.test.ts
+++ src/nua.test.ts
@@ -1,7 +1,7 @@
 import { Nua } from './nua';
 import z from 'zod';
 
-describe('map', () => {
+describe('cast/array', () => {
   test('basic response shape validation', async () => {
     const nua = new Nua();
 
@@ -52,5 +52,42 @@ describe('map', () => {
         expect(parsedFoodItem.data.food_name).toBe(inputData[index].name);
       }
     });
+  });
+});
+
+describe('cast/value', () => {
+  test('basic response shape validation', async () => {
+    const nua = new Nua();
+
+    const AddressSchema = z.object({
+      addressLine1: z.string().min(1, 'Address line 1 cannot be empty.'),
+      addressLine2: z.string().optional(),
+      city: z.string().min(1, 'City cannot be empty.'),
+      region: z.string().optional(),
+      postalCode: z.string().optional(),
+      country: z
+        .string()
+        .length(2, 'Country must be a 2-letter ISO code.')
+        .regex(/^[A-Z]{2}$/, 'Country must be a 2-letter uppercase ISO code.'),
+    });
+
+    const extractAddressFromFreeformText = nua.createFn({
+      prompt: 'Map the given text into the address schema',
+      output: {
+        name: 'address',
+        schema: AddressSchema,
+      },
+    });
+
+    const inputData = `Unit 14
+88 Lorikeet Lane
+Chatswood NSW 2067
+Australia`;
+
+    const response = await extractAddressFromFreeformText.now(inputData);
+
+    if (response.isError) throw new Error(`error in api response: ${response.error}`);
+    console.log(JSON.stringify(response, null, 2), 'JSON.stringify(response, null, 2)');
+    expect(response).toBeTruthy();
   });
 });
--- src/nua/core.ts
+++ src/nua/core.ts
@@ -1,6 +1,8 @@
 import { NuabaseAPIClient, NuabaseConfig } from '../lib/api-client';
 import { createArrayFn } from './cast_array/factory';
 import { ArrayFn, ArrayFnDef } from './cast_array/types';
+import { createValueFn } from './cast_value/factory';
+import { ValueFn, ValueFnDef } from './cast_value/types';
 import { z } from 'zod';
 
 export class Nua {
@@ -14,5 +16,11 @@ export class Nua {
     fnDef: ArrayFnDef<OutputName, OutputZodSchema>
   ): ArrayFn<OutputZodSchema, OutputName> {
     return createArrayFn(this.apiClient, fnDef);
+  }
+
+  createFn<OutputName extends string, OutputZodSchema extends z.ZodTypeAny>(
+    fnDef: ValueFnDef<OutputName, OutputZodSchema>
+  ): ValueFn<OutputZodSchema, OutputName> {
+    return createValueFn(this.apiClient, fnDef);
   }
 }
